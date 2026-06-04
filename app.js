75f79f1700a31d44c4694ca1065d96618ec4cfe0
Anupkumar Singh <anupkumarsingh@montran.com>
2026-06-04 15:49:31 +0530

    Add feature B

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("button")
         .addEventListener("click", showMessage);
 });
+
+console.log("Feature A");
+console.log("Feature B");
+
